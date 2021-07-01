0d2aa5498197450c5fe4b23352a9b00e41689c85
vybhav <svbhadri1110@gmail.com>
2021-07-02 02:26:43 +0530

    add alert

--- SmartCamera/db_models.py
+++ SmartCamera/db_models.py
@@ -1,6 +1,7 @@
 from . import db
 from flask_login import UserMixin
 
+#can add alerts as a seperate class with 1:N realtionship
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True) 
     email = db.Column(db.String(100), unique=True)
